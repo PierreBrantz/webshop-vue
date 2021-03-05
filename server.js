9f95f6776f2acfc0b21f45ccd2c7bedb7fe1a8d3
pan <pan@pan.voo.be>
2021-03-05 13:53:18 +0100

    heroku

--- server.js
+++ server.js
@@ -1,25 +1,11 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
+
 const port = process.env.PORT || 8080;
-app = express();
-app.enable("trust proxy");
-
-app.use(function(req, res, next) {
- 
-
-  
-   
-    var secureUrl = "https://webshopbepi-app.herokuapp.com";
-    res.writeHead(301, { Location: secureUrl });
-    res.end();
-  
-  next();
+app.listen(port, () => {
+  console.log('Listening on port ' + port)
 });
-
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, function(req, res) {
-  console.log("inside get");
-  res.sendFile(__dirname + "/dist/index.html");
-});
-app.listen(port);
